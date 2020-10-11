55e624b9d298018450a56d1717c2d4f28a91a935
siruollo <heretik02@gmail.com>
2020-10-11 21:19:59 +0300

    heroku

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form</title>
+</head>
+<body>
+    <?php
+    session_start();
+    include 'userInfo.php';
+    echo <<<TAG
+<p>Author: $user</p>
+TAG;
+    if (isset($_SESSION['user_name'])) {
+        echo "Hello " . $_SESSION['user_name'] . PHP_EOL;
+    echo PHP_EOL . <<<TAG
+<a href="http://exit.php">Выйти</a>
+TAG;
+    } else {
+        echo <<<TAG
+<form enctype="multipart/form-data" action="post.php" method="POST">
+        <label for="name_input">Введите имя пользователя</label>
+        <input id="name_input" type="text" name="user_name">
+        <button type="submit">Отправить</button>
+    </form>
+TAG;
+    }
+
+    ?>
+</body>
+</html>
